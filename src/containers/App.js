bc0589a94b5957bbe6b472cfc0361fcc71a34fcf
Onoriode <onosgb@gmail.com>
2020-02-13 05:16:07 +0100

    Error Handling

---
+++ src/containers/App.js
@@ -0,0 +1,43 @@
+import React, { Component}from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+class  App  extends Component  {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            seachField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event)=> {
+        this.setState({seachField: event.target.value});
+        }
+
+    render() {
+      let  {seachField, robots} = this.state;
+      const filteredRobots = robots.filter(r => {
+        return  r.name.toLowerCase().includes(seachField.toLowerCase())
+       });
+
+      return !robots.length ?    <h1>Loading</h1> : (
+            <div className="tc">
+             <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+              <Scroll>
+              <CardList robots={filteredRobots}/>
+              </Scroll> 
+            </div>
+            )
+    }  
+}
+
+export default App;
